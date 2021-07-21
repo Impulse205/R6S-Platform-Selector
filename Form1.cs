662e45a57a90f1f5733ebdd03572b29f01eaf8d0
Impulse205 <2056865483@qq.com>
2021-07-21 18:02:42 +0800

    version 1.0

---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+/*
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace R6SPlantformSwitch
+{
+    public partial class Form1 : Form
+    {
+        string pLocalFilePath;
+        string pSaveFilePath;
+        R6SPlantformSwitch.IniFiles ini;
+        public Form1()
+        {
+            InitializeComponent();
+            if(!File.Exists(Application.StartupPath + @"\config.ini"))
+            {
+                try
+                {
+                    FileStream fs = File.Create(Application.StartupPath + @"\config.ini");
+                    fs.Close();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("配置文件创建失败，原因是：\n" + e.Message, "创建失败");
+                }
+                ini = new R6SPlantformSwitch.IniFiles(Application.StartupPath + @"\config.ini");
+                ini.IniWriteValue("selection", "platform", "");
+                ini.IniWriteValue("location", "game_folder", "");
+            }
+            else
+            {
+                ini = new R6SPlantformSwitch.IniFiles(Application.StartupPath + @"\config.ini");
+                try
+                {
+                    gameFolderLoc.Text = ini.IniReadValue("location", "game_folder").Replace("/", @"\");
+                    if (gameFolderLoc.Text[gameFolderLoc.Text.Length - 1] != '\\')
+                        pSaveFilePath = gameFolderLoc.Text + @"\defaultargs.dll";
+                    else
+                        pSaveFilePath = gameFolderLoc.Text + "defaultargs.dll";
+                    pSaveFilePath = gameFolderLoc.Text + "defaultargs.dll";
+
+                    string platform = ini.IniReadValue("selection", "platform");
+                    UplayRadio.Checked = false;
+                    SteamRadio.Checked = false;
+                    if (platform == "steam")
+                    {
+                        SteamRadio.Checked = true;
+                    }
+                    else if (platform == "uplay")
+                    {
+                        UplayRadio.Checked = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // pSaveFilePath = "defaultargs.dll";
+            if (pSaveFilePath == "" || pSaveFilePath == null)
+            {
+                MessageBox.Show("文件目标位置不可用！", "目标错误");
+            }
+            else if (File.Exists(pLocalFilePath))
+            {
+                File.Copy(pLocalFilePath, pSaveFilePath, true);
+                MessageBox.Show("成功替换文件！\n已从[" + pLocalFilePath + "]\n替换到[" + pSaveFilePath + "]。", "成功");
+            }
+            else if (pLocalFilePath == "")
+            {
+                MessageBox.Show("请选择平台！", "目标错误");
+            }
+            else
+            {
+                MessageBox.Show("未找到需替换文件！请重新下载！", "缺少必要文件");
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.UplayRadio.Checked == true)
+            {
+                pLocalFilePath = Application.StartupPath + @"\uplay.dll";
+                ini.IniWriteValue("selection", "platform", "uplay");
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.SteamRadio.Checked == true)
+            {
+                pLocalFilePath = Application.StartupPath + @"\steam.dll";
+                ini.IniWriteValue("selection", "platform", "steam");
+            }
+        }
+
+        private void gameFolderLoc_TextChanged(object sender, EventArgs e)
+        {
+            gameFolderLoc.Text = gameFolderLoc.Text.Replace("/", @"\");
+            if (gameFolderLoc.Text[gameFolderLoc.Text.Length - 1] != '\\')
+                pSaveFilePath = gameFolderLoc.Text + @"\defaultargs.dll";
+            else
+                pSaveFilePath = gameFolderLoc.Text + "defaultargs.dll";
+            ini.IniWriteValue("location", "game_folder", gameFolderLoc.Text);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            gameFolderBrowser.Description = "请选择文件夹";
+            gameFolderBrowser.RootFolder = Environment.SpecialFolder.MyComputer;
+            gameFolderBrowser.ShowNewFolderButton = true;
+            if (gameFolderLoc.Text.Length > 0)
+            {
+                gameFolderBrowser.SelectedPath = gameFolderLoc.Text;
+            }
+            if (gameFolderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                gameFolderLoc.Text = gameFolderBrowser.SelectedPath;
+            }
+            pSaveFilePath = gameFolderLoc.Text + @"\defaultargs.dll";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Instructions formIns = new Instructions();
+            formIns.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "explorer";
+            proc.StartInfo.Arguments = @"/select," + gameFolderLoc.Text;
+            proc.Start();
+        }
+    }
+    public class IniFiles
+    {
+        string inipath;
+
+        public IniFiles(string path)
+        {
+            inipath = path;
+        }
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+        
+        public void IniWriteValue(string Section, string Key, string Value)
+        {
+            WritePrivateProfileString(Section, Key, Value, this.inipath);
+        }
+        public string IniReadValue(string Section, string Key)
+        {
+            StringBuilder temp = new StringBuilder(500);
+            int i = GetPrivateProfileString(Section, Key, "", temp, 500, this.inipath);
+            return temp.ToString();
+        }
+        public bool ExistINIFile()
+        {
+            return File.Exists(inipath);
+        }
+    }
+}
---
+++ Instructions.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace R6SPlantformSwitch
+{
+    partial class Instructions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "此程序是本人在更换彩六steam/uplay平台时，\r\n需要更换文件感觉很麻烦，于是制作此程序。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2021.07.21 V1.0";
+            // 
+            // Instructions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 57);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Instructions";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "说明";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
